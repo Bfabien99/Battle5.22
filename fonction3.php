0cd1c5d17922caab82b0fab76aca2c504c0717ee
Bfabien99 <91257945+Bfabien99@users.noreply.github.com>
2022-09-03 16:44:48 +0000

    fonction OK

--- fonction3.php
+++ fonction3.php
@@ -1,3 +1,25 @@
 <?php
+    function compare(array $array){
+        $positif = [];
+        $negatif = [];
+        foreach ($array as $nombre) {
+            if($nombre <= 0){
+                $negatif[] = $nombre;
+            }else{
+                $positif[] = $nombre;
+            }
+        }
 
+        if((array_sum($negatif) + array_sum($positif) < 0)){
+            $message = 'négatif';
+        }elseif((array_sum($negatif) + array_sum($positif) > 0)){
+            $message = 'positif';
+        }else{
+            $message = 'égaux';
+        }
+
+        return array_sum($positif)." et ".array_sum($negatif).", $message";
+    }
+
+    echo compare([7,-9,6,2,-8]);
 ?>
