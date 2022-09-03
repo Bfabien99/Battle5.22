85bd0e5b671299980b9dc76ff183de5b79de197c
Bfabien99 <91257945+Bfabien99@users.noreply.github.com>
2022-09-03 16:02:38 +0000

    function2 not finish

--- fonction2.php
+++ fonction2.php
@@ -1,3 +1,19 @@
 <?php
+function premier(int $nombre){
+    $nrb_diviseur = 0;
+    //$diviseur = '';
+    $premier = '';
+    if($nombre < 2){
+        return "$nombre, '1'";
+    }else{
+        $range = range(0,$nombre);
+        for ($i=0; $i < count($range) ; $i++) { 
+            
+        }
+    }
 
+    return "$premier , $nrb_diviseur";
+}
+
+echo premier(10);
 ?>
