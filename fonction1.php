d3864f811ed1a840a5a928409d43bed641a514d2
Bfabien99 <91257945+Bfabien99@users.noreply.github.com>
2022-09-03 16:30:41 +0000

    fonction OK

--- fonction1.php
+++ fonction1.php
@@ -1,3 +1,27 @@
 <?php
+// REMPLACER LES VALEURS INCONNUES PAR ZERO
+    function pythagore($AB = null, $BC = null, $AC = null){
+        if($AB != null && $BC != null && $AC != null){
+            if($AC != (($AB**2) + ($BC**2))){
+                        return 'Les valeurs ne sont pas correctes';
+                    }else{
+                        return "AC = ".sqrt($AC)."AB = ".sqrt($AB)."BC = ".sqrt($BC);
+                    }
+        }else{
+            if($AB != null && $BC != null){
+                $AC = (($AB**2) + ($BC**2));
+                return "AC = ".sqrt($AC);
+            }elseif($AB != null && $AC != null){
+                $BC = ($AC**2) - ($AB**2);
+                return "BC = ".sqrt($BC);
+            }
+            elseif($BC != null && $AC != null){
+                $AB = ($AC**2) - ($BC**2);
+                return "AB = ".sqrt($AB);
+            }else{
+                return "0";
+            }
+        }
+    }
 
-?>
+    echo pythagore(8,6,0);
